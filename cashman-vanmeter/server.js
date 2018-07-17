635ec92d8c1ebee59ef9d93cdb0f68ef73719cd8
Ben Van Meter <vanmeterx@gmail.com>
2018-07-17 13:41:22 -0500

    added listener to the bottom

--- cashman-vanmeter/server.js
+++ cashman-vanmeter/server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = (express());
 
+app.use(express.static('./public'));
+
 
 
 // REVIEW: POST route needs to parse the body passed in with the request.
@@ -17,4 +19,8 @@ app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
+})
+
+app.listen(PORT, () => {
+  console.log(PORT);
 })
